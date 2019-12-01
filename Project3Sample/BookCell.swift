4bbe64504cb4c1694b4294dafa70a5533d36c8e6
Michael Jurkoic <mjjurkoic@gmail.com>
2019-11-30 21:37:50 -0600

    Added UITableView for displaying book list.

---
+++ Project3Sample/BookCell.swift
@@ -0,0 +1,28 @@
+//
+//  BookCell.swift
+//  Project3Sample
+//
+//  Created by Michael Jurkoic on 11/30/19.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+    
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookAuthor: UILabel!
+    
+    func configure(item: Book, service: BookService, parentViewController: BookListViewController) {
+        bookTitle.text = item.title
+        bookAuthor.text = item.author
+        service.image(for: item) { [weak parentViewController] (book, image) in
+            if item.imageURL == book.imageURL {
+                DispatchQueue.main.async {
+                    self.bookImage.image = image
+                }
+            }
+        }
+    }
+}
--- Project3Sample/BookListViewController.swift
+++ Project3Sample/BookListViewController.swift
@@ -10,7 +10,42 @@ import UIKit
 
 class BookListViewController: UIViewController {
     
+    @IBOutlet weak var bookTableView: UITableView!
     
+    var books: [Book] = []
+    var bookImages = Dictionary<String, UIImage>()
+    let bookService = BookService()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bookService.fetchBooks{ [weak self] () in
+            DispatchQueue.main.async {
+                self?.bookTableView.reloadData()
+            }
+        }
+        bookTableView.dataSource = self
+        bookTableView.delegate = self
+    }
+    
+}
+
+extension BookListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = bookTableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookCell
+        cell.configure(item: books[indexPath.row], service: bookService, parentViewController: self)
+        return cell
+    }
+    
+}
+
+extension BookListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
     
 }
